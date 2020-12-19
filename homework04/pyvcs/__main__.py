cc2d4cd87f8fbd2fab8d1392d9a43f4f096395fe
mbelozerova <m25102002@gmail.com>
2020-12-19 22:08:55 +0300

    git

--- homework04/pyvcs/__main__.py
+++ homework04/pyvcs/__main__.py
@@ -5,7 +5,7 @@ from pyvcs.cli import cmd_hash_object  # type: ignore
 from pyvcs.cli import (
     cmd_cat_file,
     cmd_checkout,
-    cmd_commit_tree
+    cmd_commit_tree,
     cmd_init,
     cmd_ls_files,
     cmd_rev_parse,
